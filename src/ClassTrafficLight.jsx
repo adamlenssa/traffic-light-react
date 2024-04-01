eab77aa78ee24dd63f994bebd783cd7996686bd2
Adam Lenssa <adam.lenssa@gmail.com>
2024-04-01 19:33:02 -0400

    finished

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,40 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  state = {
+    currentColor: "red",
+  };
   render() {
+    const trafficColors = {
+      red: { next: "green", backgroundColor: "red" },
+      yellow: { next: "red", backgroundColor: "yellow" },
+      green: { next: "yellow", backgroundColor: "green" },
+    };
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          {Object.keys(trafficColors).map((color) => (
+            <div
+              className="circle black"
+              style={{
+                backgroundColor:
+                  color === this.state.currentColor &&
+                  trafficColors[color].backgroundColor,
+              }}
+            ></div>
+          ))}
         </div>
-        <button className="next-state-button">Next State</button>
+        <button
+          onClick={() => {
+            this.setState({
+              currentColor: trafficColors[this.state.currentColor].next,
+            });
+          }}
+          className="next-state-button"
+        >
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  const trafficColors = {
+    red: { next: "green", backgroundColor: "red" },
+    yellow: { next: "red", backgroundColor: "yellow" },
+    green: { next: "yellow", backgroundColor: "green" },
+  };
+  const [currentColor, setcolor] = useState("yellow");
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        {Object.keys(trafficColors).map((color) => (
+          <div
+            className="circle black"
+            style={{
+              backgroundColor:
+                color === currentColor && trafficColors[color].backgroundColor,
+            }}
+          ></div>
+        ))}
       </div>
-      <button className="next-state-button">Next State</button>
+      <button
+        onClick={() => {
+          setcolor(trafficColors[currentColor].next);
+        }}
+        className="next-state-button"
+      >
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
